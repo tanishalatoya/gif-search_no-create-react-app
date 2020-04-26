5f016d8d734300b49ec556cde35c9d87b53b6394
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-26 15:25:34 -0600

    Add fetch request to componentDidMount

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ export default class App extends Component {
     }
   } 
 
+  componentDidMount(){
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => response.json())
+      .then(data => this.setState({ gifs: [...data.data] }))
+  }
+
   render() { 
     return (
       <div>
